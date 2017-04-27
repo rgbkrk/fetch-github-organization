73f2544cd1c0c373c10b2731980a52b84096e260
Kyle Kelley <rgbkrk@gmail.com>
2017-04-27 15:38:10 -0700

    fail explicitly on bad users array

--- index.js
+++ index.js
@@ -4,13 +4,21 @@ function fetchUsers(org) {
   const tokenAuth = process.env.GITHUB_TOKEN
     ? `?access_token=${process.env.GITHUB_TOKEN}`
     : "";
+
+  if (tokenAuth) {
+    console.log("using tokenAuth");
+  }
+
   const membersURL = `https://api.github.com/orgs/${org}/members${tokenAuth}`;
-  return fetch(membersURL)
-    .then(resp => resp.json())
-    .then(users =>
-      Promise.all(
+  return fetch(membersURL).then(resp => resp.json()).then(users => {
+    // If we don't get an array of users then it was likely a failure
+    if (Array.isArray(users)) {
+      return Promise.all(
         users.map(user => fetch(user.url + tokenAuth).then(resp => resp.json()))
-      ));
+      );
+    }
+    return Promise.reject(users);
+  });
 }
 
 module.exports = fetchUsers;
